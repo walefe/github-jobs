9d071e818dd36271e3ea4e1aeb38c22d8dd390a9
Walefe <walefedev@gmail.com>
2021-02-17 20:22:38 -0300

    refact: removed layout component from app and index and refact css

--- src/components/Layout/styles.ts
+++ src/components/Layout/styles.ts
@@ -4,11 +4,11 @@ export const Container = styled.div`
   width: 144rem;
   max-height: 100%;
   max-width: 100%;
+  margin: 0 auto;
   padding: 3.2rem 12rem;
   background-color: #f6f7fb;
 
   @media (max-width: 26rem) {
     padding: 1.2rem;
-    font-size: 1.2rem;
   }
 `;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
 import { FC } from 'react';
 
-const Home: FC = () => <p>Hello Next</p>;
+import Header from '@/components/Header';
+import Layout from '@/components/Layout';
+
+const Home: FC = () => (
+  <Layout>
+    <Header />
+  </Layout>
+);
 
 export default Home;
