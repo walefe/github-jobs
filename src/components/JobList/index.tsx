f205f44578bb3b686baba2ab07cf178aef10f63a
Walefe <walefedev@gmail.com>
2021-03-05 15:02:33 -0300

    feat: use hook for consumer data.

--- src/components/JobList/index.tsx
+++ src/components/JobList/index.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 
 import JobCard from '@/components/JobCard';
+import Loading from '@/components/Loading';
 
+import { useJob } from '@/hooks';
 import { Container } from './styles';
 
-const JobList: React.FC = () => (
-  <Container>
-    <JobCard />
-    <JobCard />
-    <JobCard />
-    <JobCard />
-    <JobCard />
-  </Container>
-);
+const JobList: React.FC = () => {
+  const { data, isLoading, isError } = useJob();
+
+  return (
+    <Container>
+      <Loading loading={isLoading} error={isError}>
+        {data.map(job => (
+          <JobCard key={job.id} job={job} />
+        ))}
+      </Loading>
+    </Container>
+  );
+};
 
 export default JobList;
--- src/components/JobList/styles.ts
+++ src/components/JobList/styles.ts
@@ -6,5 +6,4 @@ export const Container = styled.section`
   align-items: flex-end;
   max-width: 73rem;
   width: 100%;
-  height: 79rem;
 `;
